6d4160b7b58cff5fc8b381937b5df89790cd357d
misgna <xyhaile@yahoo.com>
2014-06-18 11:10:23 +0200

    Update tags_from_review.py

--- tags_from_review.py
+++ tags_from_review.py
@@ -3,7 +3,7 @@
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
 
-def features():
+def tags():
 	file_list = ['part-m-00000','part-m-00001','part-m-00002','part-m-00003','part-m-00004','part-m-00005','part-m-00006','part-m-00007','part-m-00008']
 	for flist in file_list:
 		with open('review_rating/' + flist) as f:
@@ -17,4 +17,4 @@ def features():
 
 #main method
 if __name__ == "__main__":
-	features()
+	tags()
